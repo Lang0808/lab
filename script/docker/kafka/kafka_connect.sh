2fe426122d84c0e5d833bdf15c2a808c6b908602
TywinLannister <Minhbaodinh0808@gmail.com>
2025-09-29 23:15:02 +0700

    Integrate Kafka Connect with Kpow

--- script/docker/kafka/kafka_connect.sh
+++ script/docker/kafka/kafka_connect.sh
@@ -1,6 +1,19 @@
 docker stop kafka_connect || true
 docker rm kafka_connect || true
 
+PLUGIN_PATH='/usr/kafka/plugin'
+JARS_PATH='/etc/kafka-connect/jars'
+SECRET_PATH='/etc/kafka-connect/secrets'
+SECRET_KAFKA_PATH='/etc/kafka/secrets'
+DATA_KAFKA='/var/lib/kafka/data'
+
+PROJECT_PLUGIN_PATH="$(pwd)/../../../config/kafka/connect/plugin"
+
+docker volume create --name kafka_connect_jars || true
+docker volume create --name kafka_connect_secrets || true
+docker volume create --name kafka_secret1 || true
+docker volume create --name kafka_connect_data || true
+
 docker run -d \
   --name kafka_connect \
   -e CONNECT_CONFIG_STORAGE_TOPIC=kafka_connect_config \
@@ -10,7 +23,7 @@ docker run -d \
   -e CONNECT_KEY_CONVERTER=org.apache.kafka.connect.json.JsonConverter \
   -e CONNECT_VALUE_CONVERTER=org.apache.kafka.connect.json.JsonConverter \
   -e CONNECT_BOOTSTRAP_SERVERS=host.docker.internal:19092 \
-  -e CONNECT_PLUGIN_PATH=/usr/kafka/plugin \
+  -e CONNECT_PLUGIN_PATH="/${PLUGIN_PATH}" \
   -e CONNECT_LISTENERS=http://0.0.0.0:18080 \
   -e CONNECT_REST_ADVERTISED_HOST_NAME=host.docker.internal \
   -e CONNECT_REST_PORT=18080 \
@@ -18,4 +31,9 @@ docker run -d \
   -e CONNECT_STATUS_STORAGE_REPLICATION_FACTOR=1 \
   -e CONNECT_OFFSET_STORAGE_REPLICATION_FACTOR=1 \
   -p 18080:18080 \
+  --mount type=bind,src="${PROJECT_PLUGIN_PATH}",dst="${PLUGIN_PATH}" \
+  -v kafka_connect_jars:${JARS_PATH} \
+  -v kafka_connect_secrets:${SECRET_PATH} \
+  -v kafka_secret1:${SECRET_KAFKA_PATH} \
+  -v kafka_connect_data:${DATA_KAFKA} \
   confluentinc/cp-kafka-connect:7.7.5
--- script/docker/kafka/kpow.sh
+++ script/docker/kafka/kpow.sh
@@ -1,10 +1,20 @@
 docker stop kpow || true
 docker rm kpow || true
 
+LICENSE_SIGNATURE=$(cat ../../../config/kpow/license_signature)
+
 docker run \
   -d \
   --name kpow \
   -p 3000:3000 \
-  --env-file ../../../config/kpow/config.env \
+  -e ENVIRONMENT_NAME="Lab Kafka Monitor" \
+  -e BOOTSTRAP="host.docker.internal:19092" \
+  -e LICENSE_ID=dd1633cf-b1a4-45fa-a7f0-b684ed739b24 \
+  -e LICENSE_CODE=KPOW_COMMUNITY \
+  -e LICENSEE="Dinh Bao" \
+  -e LICENSE_EXPIRY=2026-09-24 \
+  -e LICENSE_SIGNATURE="${LICENSE_SIGNATURE}" \
+  -e CONNECT_NAME=LUKE \
+  -e CONNECT_REST_URL=http://host.docker.internal:18080 \
   -m 2G \
   factorhouse/kpow-ce:94.5
